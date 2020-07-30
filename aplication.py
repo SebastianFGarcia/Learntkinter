6b97bd493097a30d00c263e97c0c22b2e6703c1b
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2020-07-30 16:37:54 -0500

    Iniciando una calculadora

--- aplication.py
+++ aplication.py
@@ -5,9 +5,17 @@ root = Tk()
 ###myLabel1 = Label(root, text="hello wold").grid(row=0, column=0)
 ###myLabel2 = Label(root, text="hello wold").grid(row=1, column=2)
 ###myLabel3 = Label(root, text="hello wold").grid(row=2, column=3)
+
+#Creando Cajas de Texto
+#BG = Background
+#Borderwidth = Borde
+e = Entry(root, width=50)
+e.pack()
+e.insert(0,"Escriba algo")
+#e.get() extrae el texto
 #Creando botones
 def myClick():
-    myLabel1 = Label(root, text="hello wold")
+    myLabel1 = Label(root, text=e.get())
     myLabel1.pack()
 # Text= texto del boton
 # Padx=ancho del boton
@@ -15,7 +23,8 @@ def myClick():
 # Command = Funciones que hace el boton
 # Fg = colores de las letras
 # Bg = Background del boton
-myButton = Button(root, text="hola", padx=50, pady= 10, command= myClick, fg="blue", bg="green")
+#myButton = Button(root, text="hola", padx=50, pady= 10, command= myClick, fg="blue", bg="green")
+myButton = Button(root, text="hola", command= myClick)
 myButton.pack()
 
 root.mainloop()
---
+++ calculator.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+root.title("Calculadora")
+
+Ctext = Entry(root,width=50)
+Button1 = Button(root,text="1").pack()
+Button2 = Button(root,text="1").pack()
+Button3 = Button(root,text="1").pack()
+Button4 = Button(root,text="1").pack()
+Button5 = Button(root,text="1").pack()
+Button6 = Button(root,text="1").pack()
+Button7 = Button(root,text="1").pack()
+Button8 = Button(root,text="1").pack()
+Button9 = Button(root,text="1").pack()
+
+
+root.mainloop()
