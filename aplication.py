0741ec560b75b0badc292a406e962f04081534ec
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2020-07-29 20:44:04 -0500

    Labels y Buttons

--- aplication.py
+++ aplication.py
@@ -2,15 +2,20 @@ from tkinter import *
 
 root = Tk()
 #Creando labels
-#myLabel1 = Label(root, text="hello wold").grid(row=0, column=0)
-#myLabel2 = Label(root, text="hello wold").grid(row=1, column=2)
-#myLabel3 = Label(root, text="hello wold").grid(row=2, column=3)
+###myLabel1 = Label(root, text="hello wold").grid(row=0, column=0)
+###myLabel2 = Label(root, text="hello wold").grid(row=1, column=2)
+###myLabel3 = Label(root, text="hello wold").grid(row=2, column=3)
 #Creando botones
 def myClick():
     myLabel1 = Label(root, text="hello wold")
     myLabel1.pack()
-
-myButton = Button(root, text="hola", padx=50, pady= 10, command= myClick )
+# Text= texto del boton
+# Padx=ancho del boton
+# Pady = alto del boton
+# Command = Funciones que hace el boton
+# Fg = colores de las letras
+# Bg = Background del boton
+myButton = Button(root, text="hola", padx=50, pady= 10, command= myClick, fg="blue", bg="green")
 myButton.pack()
 
 root.mainloop()
