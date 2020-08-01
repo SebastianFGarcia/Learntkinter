eeac50e7269000a837d325979f39870b38e69ccf
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2020-07-31 21:50:37 -0500

    Aplicando mensajes Al usuario

---
+++ MessageBox.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title("Cuadros de Mensajes")
+root.iconbitmap('imagenes/python.ico')
+
+#showinfo, showwarning, showerror, askquestion, askokcancel, askyesno
+
+def popup():
+    response = messagebox.askyesnocancel("Este es mi popup!","Hello world!")
+    if response == 1:
+        Label(root, text="yes").pack()
+    elif response == 0:
+        Label(root, text="no").pack()
+    else:
+        root.quit()
+
+Button(root, text="Popup", command=popup).pack()
+
+root.mainloop()
