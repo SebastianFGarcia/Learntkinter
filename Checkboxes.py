4804ca134c305c610297047a9dd676c909c4ab54
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2020-08-01 22:34:47 -0500

    Usando Checkboxes

---
+++ Checkboxes.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+root = Tk()
+root.title("Checkboxes")
+root.iconbitmap("imagenes/python.ico")
+root.geometry("400x400")
+
+
+def show():
+    mylabel = Label(root, text=var.get()).pack()
+
+
+var = StringVar()
+
+c = Checkbutton(root, text="Check this box", variable=var, onvalue="On", offvalue="Off")
+c.deselect()
+c.pack()
+
+myButton = Button(root, text="show Selection", command=show).pack()
+
+
+mainloop()
