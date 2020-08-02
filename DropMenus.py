69b9b83a4651579800501165568d944140183ed5
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2020-08-02 12:53:34 -0500

    Drop Menus

---
+++ DropMenus.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.iconbitmap("imagenes/python.ico")
+root.title("Drop Menus")
+root.geometry("400x400")
+
+def show():
+    myLabel = Label(root, text=clicked.get()).pack()
+
+
+options = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", ]
+clicked = StringVar()
+clicked.set(options[0])
+drop = OptionMenu(root, clicked, *options)
+drop.pack()
+
+myButton = Button(root, text="Click me!", command=show).pack()
+
+mainloop()
